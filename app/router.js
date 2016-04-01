810b93a35ede0658a2b1a970bae3d7c785335ccf
simon <simonctemple@live.com>
2016-04-01 14:45:08 -0700

    have same functionality, working on adding answers to questions

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('question', {path: '/question/:question_id'});
+  this.route('answer', {path: '/answer/:answer_id'});
 });
 
 export default Router;
