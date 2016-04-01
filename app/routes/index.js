27476f046f1716f97eed83bd39cc77bd8d2ba476
simon <simonctemple@live.com>
2016-04-01 09:28:26 -0700

    add firebase data and delete functionality

---
+++ app/routes/index.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+
+  actions: {
+    destroyQuestion(question) {
+      question.destroyQuestion();
+      this.transitionTo('index');
+    }
+  }
+});
