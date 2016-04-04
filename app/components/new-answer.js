7b7d87eadcbb7ec7f8702cba40fa0e645eaacbd1
Simon Temple <simonctemple@live.com>
2016-04-03 23:50:54 -0700

    saves author but not answer content

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addAnswerForm: false,
+
+  actions: {
+    answerFormShow() {
+      this.set('addAnswerForm', true);
+    },
+
+    saveAnswer(){
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        question: this.get('question'),
+      };
+      this.set('addAnswerForm', false);
+      this.sendAction('saveAnswer2', params);
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -15,9 +15,27 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
+    destroyQuestion(question){
+      if(confirm('Delete this question?')){
+        var answer_deletions = question.get('answer').map(function(answer){
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function(){
+          return question.destroyRecord();
+        });
+        this.transitionTo('index');
+      }
+    },
+
+    saveAnswer3(params){
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+
+      question.get('answer').addObject(newAnswer);
+      newAnswer.save().then(function(){
+        return question.save();
+      });
+      this.transitionTo('question', params.question);
     }
   }
 });
