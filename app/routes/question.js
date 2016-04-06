f2b0bf7ad8373f19d03c5ca86fadbdd045348cb5
Simon Temple <simonctemple@live.com>
2016-04-05 19:19:30 -0700

    fix add answers bug so it stores in database

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,38 +5,37 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    // update(question, params) {
-    //   Object.keys(params).forEach(function(key) {
-    //     if(params[key]!==undefined) {
-    //       question.set(key,params[key]);
-    //     }
-    //   });
-    //   console.log(question)
-    //   question.save();
-    //   this.transitionTo('index');
-    // },
-    //
-    // destroyQuestion(question){
-    //   if(confirm('Delete this question?')){
-    //     var answer_deletions = question.get('answer').map(function(answer){
-    //       return answer.destroyRecord();
-    //     });
-    //     Ember.RSVP.all(answer_deletions).then(function(){
-    //       return question.destroyRecord();
-    //     });
-    //     this.transitionTo('index');
-    //   }
-    // },
-
-    saveAnswer3(params){
-      var newAnswer = this.store.createRecord('answer', params);
-      var question = params.question;
-
-      question.get('answer').addObject(newAnswer);
-      newAnswer.save().then(function(){
-        return question.save();
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
       });
-      this.transitionTo('question', params.question);
-    }
+      console.log(question)
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question){
+      if(confirm('Delete this question?')){
+        var answer_deletions = question.get('answer').map(function(answer){
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function(){
+          return question.destroyRecord();
+        });
+        this.transitionTo('index');
+      }
+    },
+
+    saveAnswer3(params) {
+       var newAnswer = this.store.createRecord('answer', params);
+       var question = params.question;
+       question.get('answers').addObject(newAnswer);
+       newAnswer.save().then(function() {
+         return question.save();
+       });
+       this.transitionTo('question', params.question);
+     },
   }
 });
