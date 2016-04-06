a0bd2e74de93f4b4a7d4b6529c02c03fd9708a3b
Simon Temple <Guest@epicodus-63.local>
2016-04-05 17:30:47 -0700

    fix bugs saving and displaying answers

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
     saveAnswer(){
       var params = {
         author: this.get('author'),
-        content: this.get('content'),
+        text: this.get('text'),
         question: this.get('question'),
       };
       this.set('addAnswerForm', false);
--- app/routes/question.js
+++ app/routes/question.js
@@ -5,27 +5,28 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    },
-
-    destroyQuestion(question){
-      if(confirm('Delete this question?')){
-        var answer_deletions = question.get('answer').map(function(answer){
-          return answer.destroyRecord();
-        });
-        Ember.RSVP.all(answer_deletions).then(function(){
-          return question.destroyRecord();
-        });
-        this.transitionTo('index');
-      }
-    },
+    // update(question, params) {
+    //   Object.keys(params).forEach(function(key) {
+    //     if(params[key]!==undefined) {
+    //       question.set(key,params[key]);
+    //     }
+    //   });
+    //   console.log(question)
+    //   question.save();
+    //   this.transitionTo('index');
+    // },
+    //
+    // destroyQuestion(question){
+    //   if(confirm('Delete this question?')){
+    //     var answer_deletions = question.get('answer').map(function(answer){
+    //       return answer.destroyRecord();
+    //     });
+    //     Ember.RSVP.all(answer_deletions).then(function(){
+    //       return question.destroyRecord();
+    //     });
+    //     this.transitionTo('index');
+    //   }
+    // },
 
     saveAnswer3(params){
       var newAnswer = this.store.createRecord('answer', params);
